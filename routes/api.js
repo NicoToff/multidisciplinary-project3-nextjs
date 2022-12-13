8a69f91324b6a5dca45bd204e1e86b771aee6583
NicoToff <toffolo01@hotmail.com>
2022-12-13 14:34:04 +0100

    Initial commit

---
+++ routes/api.js
@@ -0,0 +1,50 @@
+var express = require("express");
+var router = express.Router();
+
+const mqttDomain = "178.32.223.217";
+const port = 80;
+const mqttUri = `mqtt://${mqttDomain}`;
+const options = {
+    username: "groupe2",
+    password: "groupe2",
+    port,
+    keepalive: 60,
+};
+
+const mqtt = require("mqtt").connect(mqttUri, options);
+
+mqtt.on("connect", () => {
+    console.log(`Connected to ${mqttUri}`);
+    mqtt.subscribe("/groupe2/#", err => {
+        if (!err) {
+            console.log("Subscribed to /groupe2/#");
+        } else {
+            console.log("Error while subscribing to /groupe2/#");
+        }
+    });
+});
+
+const map = new Map();
+mqtt.on("message", (topic, message) => {
+    // console.log(`${topic}: ${message}`);
+    map.set(topic, message.toString());
+});
+
+/* GET users listing. */
+router.get("/", function (req, res, next) {
+    // Make a JSON array from the map
+    const json = jsonify(map);
+    console.log(json);
+
+    res.json(json);
+});
+
+function jsonify(map) {
+    return [...map.entries()].reduce((acc, [topic, message]) => {
+        const nKey = topic.replace("/groupe2/", "");
+        acc[nKey] = message;
+        return acc;
+    }, {});
+}
+
+module.exports = router;
